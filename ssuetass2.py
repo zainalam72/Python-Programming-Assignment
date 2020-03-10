22ba18063c0ac1c61e4c036eef25b448db113d4e
zainalam72 <zainalam72@hotmail.com>
2020-03-10 23:33:30 +0500

    python first task

---
+++ ssuetass2.py
@@ -0,0 +1,78 @@
+# Write a Python program which accepts the radius of a circle from the user and compute the area
+
+# print("Compute The Area")
+# rd = int(input("Enter Circle's Radius :"))
+# rad = rd*rd*3.142
+# print(rad)
+
+# Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+# name = str(input("Enter Your Name :"))
+# age = int(input("Enter Your Age :"))
+# age1 = 100-age
+# print("{} year that you will turn 100 yrs old".format(age1))
+
+
+# Ask the user for a number. Depending on whether the number is even or odd,
+# print out an appropriate message to the user.
+
+# num = int(input("Enter Number :"))
+# if num % 2 == 0:
+#     print("The Number is Even")
+# else:
+#     print("The Number is Odd")
+
+
+# Imagine an alien was just shot down in a game.
+# Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+# •	Write an if statement to test whether the alien’s color is green. If it is,
+# print a message that the player just earned 5 points.
+# •	Write one version of this program that passes the if test and another that fails.
+#  (The version that fails will have no output.)
+
+# alien_color = "green"
+# alien_color = "yellow"
+# alien_color = "red"
+# if alien_color == "green":
+#     print("You earned 5 points")
+# if alien_color == "purple":
+#     pass
+
+
+# Choose a color for an alien as you did in Exercise 3, and write an if-else chain.
+# •	If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+# •	If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+# •	Write one version of this program that runs the if block and another that runs the else block.
+
+
+# alien_color = "green"
+# alien_color = "yellow"
+# alien_color = "red"
+# if alien_color == "green":
+#     print("You earned 5 points for shooting the alien")
+# if alien_color != "green":
+#     print("You just earned 10 points")
+
+
+# Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+# •	If the person is less than 2 years old, print a message that the person is a baby.
+# •	If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+# •	If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+# •	If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+# •	If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+# •	If the person is age 65 or older, print a message that the person is an elder
+
+# person = int(input("Enter Your Age"))
+# if person <= 2:
+#     print("The person is a baby")
+# elif person < 4:
+#     print("The person is a toddler")
+# elif person < 13:
+#     print("The person is a kid")
+# elif person < 20:
+#     print("The person is a teenager")
+# elif person < 65:
+#     print("The person is a adult")
+# else:
+#     print("The person is a elder")
